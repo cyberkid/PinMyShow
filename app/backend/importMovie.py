126319664f958dfcb8d2af7e83aa43e950fefa67
lenovo-laptop <pinmyshow@gmail.com>
2014-05-03 22:09:41 +0530

    test py

--- app/backend/importMovie.py
+++ app/backend/importMovie.py
@@ -2,10 +2,9 @@ from omdb import *
 from trakt import *
 import sys
 
-
 def import_movie(start, offset):
     for x in range(int(start), int(start)+int(offset)):
-        print "start=", start, "end=", start+offset
+        print "start=", start, "end=", int(start)+int(offset)
         zeroes = 7 - len(str(x))
         extra=""
         for i in range(0, zeroes):
@@ -14,6 +13,8 @@ def import_movie(start, offset):
         print imdb_id
         omdb = omdb_get_data(imdb_id)
         trakt = trakt_get_data(imdb_id)
+        file=open("imported",'w')
+        file.write(imdb_id)
         print omdb, "\n", trakt
 
 import_movie(sys.argv[1], sys.argv[2])
