e13b3abc9882cd163b151eedf1ead37d622b6588
lenovo-laptop <pinmyshow@gmail.com>
2014-05-03 21:56:27 +0530

    test py

--- app/backend/omdb.py
+++ app/backend/omdb.py
@@ -12,6 +12,7 @@ def online_lookup(imdb_id):
     else:
         url = 'http://www.omdbapi.com/?i=tt' + imdb_id
     omdb = json.loads(requests.get(url).content)
+    print omdb
     store_one_movie('imdbID', omdb, Config.COLLECTION_OMDB)
     return omdb
 
