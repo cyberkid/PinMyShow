6d4a6c316205005cbe358fc9c14d08f64982171a
Cyberkid <vishvesh91@live.com>
2014-05-03 18:56:11 +0530

    enabled logging

--- app/backend/actions.py
+++ app/backend/actions.py
@@ -6,6 +6,6 @@ def store_movies(movies, collection_name):
     client = MongoClient()
     db = client[Config.DB_MOVIES]
     collection = db[collection_name]
-    for movie in movies:
-        movie['_id'] = movie['id']
-        collection.save(movie)
+  #  for movie in movies:
+  #      movie['_id'] = movie['id']
+  #      collection.save(movie)
