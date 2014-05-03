ae07b0271c51db64e7ca1364940fabff36622a44
lenovo-laptop <pinmyshow@gmail.com>
2014-05-03 22:14:31 +0530

    test py

--- app/backend/importMovie.py
+++ app/backend/importMovie.py
@@ -11,8 +11,14 @@ def import_movie(start, offset):
             extra += "0"
         imdb_id = "tt"+extra+str(x)
         print imdb_id
-        omdb = omdb_get_data(imdb_id)
-        trakt = trakt_get_data(imdb_id)
+        try:
+            omdb = omdb_get_data(imdb_id)
+        except:
+            pass
+        try:
+            trakt = trakt_get_data(imdb_id)
+        except:
+            pass
         file=open("imported",'w')
         file.write(imdb_id)
         print omdb, "\n", trakt
