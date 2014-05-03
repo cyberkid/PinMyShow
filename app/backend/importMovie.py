38e6d24a70778e2b7a7ba51f3bdad5f3a14e7191
lenovo-laptop <pinmyshow@gmail.com>
2014-05-03 21:40:39 +0530

    test py

--- app/backend/importMovie.py
+++ app/backend/importMovie.py
@@ -6,6 +6,7 @@ import sys
 def import_movie(start, offset):
     for x in range(int(start), int(start + offset)):
         imdb_id = "tt" + str(x)
+        print imdb_id;
         omdb = omdb_get_data(imdb_id)
         trakt = trakt_get_data(imdb_id)
         print omdb, "\n", trakt
