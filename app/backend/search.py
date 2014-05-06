003df8d6c5dfd196f8deab3ad37011305c0c39f2
lenovo-laptop <pinmyshow@gmail.com>
2014-05-06 12:32:00 +0530

    removed only trakt

--- app/backend/search.py
+++ app/backend/search.py
@@ -17,7 +17,7 @@ def get_detailed_movies(movies):
             item['rt_id'] = movie['id']
             item['imdb_id'] = movie['alternate_ids']['imdb']
             #tmp['trakt'] = trakt_get_data(movie['alternate_ids']['imdb'])
-            #tmp['omdb'] = omdb_get_data(movie['alternate_ids']['imdb'])
+            tmp['omdb'] = omdb_get_data(movie['alternate_ids']['imdb'])
         except Exception, e:
             failed_omdb = open("/home/ubuntu/failed_omdb", "a")
             failed_omdb.write(str(e) + "\n")
