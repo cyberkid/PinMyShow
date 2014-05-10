25af85891100afb9e23158d6a4319f1f691e8f46
pinmyshow <pinmyshow@gmail.com>
2014-05-10 15:24:47 +0530

    adding access token

--- app/backend/register.py
+++ app/backend/register.py
@@ -5,6 +5,7 @@ import base64
 from pymongo import MongoClient
 from config import Config
 from hashlib import sha256, sha1
+import os
 
 from raven.handlers.logging import SentryHandler
 from raven import Client
@@ -14,10 +15,10 @@ import datetime
 
 
 
-# client = Client('https://c08e468ddcf148d3bed9966345bdb7f4:5c4227f8d4fd4b1e94d01ebe03e29883@app.getsentry.com/23855')
-# handler = SentryHandler(client)
-# setup_logging(handler)
-# logger = logging.getLogger(__name__)
+client = Client('https://c08e468ddcf148d3bed9966345bdb7f4:5c4227f8d4fd4b1e94d01ebe03e29883@app.getsentry.com/23855')
+handler = SentryHandler(client)
+setup_logging(handler)
+logger = logging.getLogger(__name__)
 
 class RegisterUser(Resource):
     def post(self):
@@ -27,11 +28,13 @@ class RegisterUser(Resource):
         http_code = 201
         db = client[Config.DB_PMS]
         collection = db[Config.COLLECTION_USERS]
-        # auth_token=request_params['auth_token']
-        # logger.warning("Register/Update user: %s, auth_token: %s",request_params['email'],auth_token)
+        auth_token=request_params['auth_token']
+        logger.warning("Register/Update user: %s, auth_token: %s",request_params['email'],auth_token)
         status['token'] = base64.b64encode(sha1(request_params['email']).hexdigest())
         request_params['_id'] = sha256(request_params['email']).hexdigest()
         check = collection.find({'email':request_params['email']})
+        acccess_token=base64.urlsafe_b64encode(os.urandom(30))
+        request_params['access_token']=acccess_token
         if check.count() > 0:
             status = {'status_code':200, 'message': 'Successfully Updated'}
             http_code = 200
@@ -39,5 +42,5 @@ class RegisterUser(Resource):
         else:
             request_params['join_date'] = datetime.datetime.utcnow()
             create_id = collection.save(request_params)
-        return status, http_code
+        return status, http_code,
 
