c4803f2ea9ce08e0ccc9a738bea67a751148bb90
pinmyshow <pinmyshow@gmail.com>
2014-05-11 01:18:32 +0530

    access_token_matches bugfix

--- app/backend/actions.py
+++ app/backend/actions.py
@@ -29,7 +29,7 @@ def access_token_matches(email_id,access_token):
     client=MongoClient()
     db=client[Config.DB_PMS]
     collection=db[Config.COLLECTION_USERS]
-    result=collection.find_one({'email',email_id})
+    result=collection.find_one({'email':email_id})
     if result == None:
         return False
     elif result['access_token'] in access_token:
