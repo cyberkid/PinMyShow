c47b94abb9fbcb30c28acffe166aad78f9094866
pinmyshow <pinmyshow@gmail.com>
2014-05-10 18:10:49 +0530

    changed mypins

--- app/backend/rt.py
+++ app/backend/rt.py
@@ -56,7 +56,7 @@ def db_lookup(imdb_id):
 def db_lookup_rt(rt_id):
     client = MongoClient()
     db = client[Config.DB_MOVIES]
-    collection = db[Config.COLLECTION_OMDB]
+    collection = db[Config.COLLECTION_RT]
     return collection.find_one({'_id': rt_id})
 
 
