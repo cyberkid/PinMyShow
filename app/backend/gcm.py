138009141f8e11e4e6b7ac3ed26c4a2a38e71519
pinmyshow <pinmyshow@gmail.com>
2014-05-19 21:16:17 +0530

    adding gcm support

--- app/backend/gcm.py
+++ app/backend/gcm.py
@@ -33,5 +33,5 @@ class GCM(Resource):
             return {'status':400,'message':'Bad Request'},400
         elif access_token_matches(email_id,access_token) == False:
             return {'status':401,'message':'Access Unauthorized'},401
-        response={'status':200,'message':sendNotification(gcm_id,message)}
+        response={'status':200,'message':'success'}
         return response
