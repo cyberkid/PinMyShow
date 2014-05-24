69fcb275d1c14a0d87da1708158e184c8804465e
pinmyshow <pinmyshow@gmail.com>
2014-05-24 11:18:23 +0530

    doing youtube feed parsing for trailer

--- app/backend/trailer.py
+++ app/backend/trailer.py
@@ -6,6 +6,16 @@ from flask import request
 from actions import ts_signature_validation
 from BeautifulSoup import BeautifulSoup
 
+from raven.handlers.logging import SentryHandler
+from raven import Client
+from raven.conf import setup_logging
+import logging
+
+client = Client('https://c08e468ddcf148d3bed9966345bdb7f4:5c4227f8d4fd4b1e94d01ebe03e29883@app.getsentry.com/23855')
+handler = SentryHandler(client)
+setup_logging(handler)
+logger = logging.getLogger(__name__)
+
 
 class Trailers(Resource):
     def get(self, search_string):
@@ -43,17 +53,28 @@ class Trailer(Resource):
         #if ts ==None or signature==None or ts_signature_validation(ts,signature)==False:
         #    return {'status':401,'message':'Invalid signature'},401
 
-        #default = "/default.jpg"
-        #hq = "/hqdefault.jpg"
-        #mq = "/mqdefault.jpg"
-        #sd = "/sddefault.jpg"
-        #max = "/maxresdefault.jpg"
+        default = "/default.jpg"
+        hq = "/hqdefault.jpg"
+        mq = "/mqdefault.jpg"
+        sd = "/sddefault.jpg"
+        max = "/maxresdefault.jpg"
         #
         source_url = "http://gdata.youtube.com/feeds/api/videos?q=" + search_string + "+official+trailer"
         feed = requests.get(source_url)
         data=feedparser.parse(feed.content)
         response={}
+        response['title']=search_string
         if len(data.entries) > 0:
-            feed=data.entries[0];
-            response['link']=feed.link.replace("&feature=youtube_gdata","")
+            try:
+                feed=data.entries[0];
+                response['trailer']=feed.link.replace("&feature=youtube_gdata","")
+                thumb_path = feed.media_thumbnail[0]['url'].rsplit("/", 1)[0]
+                response["defaultThumb"] = thumb_path + default
+                response["hqThumb"] = thumb_path + hq
+                response["mqThumb"] = thumb_path + mq
+                response["sdThumb"] = thumb_path + sd
+                response["maxThumb"] = thumb_path + max
+            except Exception as e:
+                logger.error(e)
+                pass
         return response,200
