3f3ca858f114b6ea0b45712c71b31d29e5a14ddf
pinmyshow <pinmyshow@gmail.com>
2014-05-24 13:20:38 +0530

    doing showtimes

---
+++ app/backend/movie_in_theatre.py
@@ -0,0 +1,28 @@
+from flask_restful import Resource
+from flask import request
+import requests
+
+from BeautifulSoup import BeautifulSoup
+import re
+
+from raven.handlers.logging import SentryHandler
+from raven import Client
+from raven.conf import setup_logging
+import logging
+
+client = Client('https://c08e468ddcf148d3bed9966345bdb7f4:5c4227f8d4fd4b1e94d01ebe03e29883@app.getsentry.com/23855')
+handler = SentryHandler(client)
+setup_logging(handler)
+logger = logging.getLogger(__name__)
+
+
+class MovieInTheatre(Resource):
+    def get(self,movie):
+        url="http://www.google.com/movies?near=bangalore&q="+movie;
+        resp=requests.get(url)
+        content=re.findall(r"\<div\>.*\<\/div\>",resp.content)
+        soup =BeautifulSoup(content)
+        return content,200
+
+
+
